723e8af5ebf71385fe8909f99439dd36bb50025e
Richard Widtmann <rwidtmann@sbcglobal.net>
2020-07-15 08:34:30 -0500

    Adding home-routes updates and login handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,18 +41,8 @@ router.get('/', (req, res) => {
 });
 
 
-// router.get('/', (req, res) => {
-//     res.render('homepage', {
-//         id: 1,
-//         post_url: 'https://handlebarsjs.com/guide/',
-//         title: 'Handlebars Docs',
-//         created_at: new Date(),
-//         vote_count: 10,
-//         comments: [{}, {}],
-//         user: {
-//             username: 'test_user'
-//         }
-//     });
-// });
+router.get('/login', (req, res) => {
+    res.render('login');
+});
 
 module.exports = router;
